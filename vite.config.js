fdd3a04b40815973f0ccdf1efcf4f24fa3bd745d
meeyth <anurupbhowmick2003@gmail.com>
2026-02-04 13:41:49 +0530

    update vite.config commit

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    host: true, // or '0.0.0.0' for network access
-  },
+  base: './'
 });
